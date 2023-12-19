f61d51a40eb9216c9b522f157453784852723769
@Laiiif <danimatto610@gmail.com>
2023-12-19 16:41:21 +0100

    Initial commit

---
+++ src/test/java/com/example/Autenticazione/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Autenticazione.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
